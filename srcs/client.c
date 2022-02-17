8deb77839f47f742eed19f83cf6561de40a8dc85
tomori <tmrtkm1227tksx@gmail.com>
2022-02-18 01:37:46 +0900

    [add] ft_send_msg function

--- srcs/client.c
+++ srcs/client.c
@@ -6,31 +6,46 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:51:04 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/18 00:21:39 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/18 01:35:54 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_send_char(int pid, unsigned char uc)
+void	ft_send_char(pid_t pid, char c)
 {
-	int	bit;
-	int	offset;
-	int	result;
+	int				bit;
+	int				res;
+	int				offset;
+	unsigned char	uc;
 
 	offset = 7;
+	uc = (unsigned char)c;
 	while (0 <= offset)
 	{
+		usleep(50);
 		bit = uc & (0x01 << offset);
 		if (bit == 0x00)
-			result = kill(pid, SIGUSR1);
+			res = kill(pid, SIGUSR1);
 		else
-			result = kill(pid, SIGUSR2);
-		if (result == -1)
+			res = kill(pid, SIGUSR2);
+		if (res == -1)
 		{
 			ft_printf("kill %d failed: no such process.\n", pid);
 			exit(1);
 		}
 		offset--;
+	}
+}
+
+void	ft_send_msg(pid_t pid, char *msg)
+{
+	size_t	i;
+
+	i = 0;
+	while (msg[i] != '\0')
+	{
+		ft_send_char(pid, msg[i]);
+		i++;
 	}
 }
