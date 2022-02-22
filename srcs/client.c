46ea876000a2f3d7de421f1ac6f61df8e1804da0
tomori <tmrtkm1227tksx@gmail.com>
2022-02-22 11:33:00 +0900

    [update] Check existence of process

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:51:04 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/22 11:23:12 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/22 11:29:46 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,12 @@ int	main(int ac, char **av)
 	}
 	signal(SIGUSR1, sig_handler);
 	pid = parse_pid(av[1]);
-	if (pid < 0)
+	if (pid < 0 || kill(pid, 0) != 0)
 	{
-		ft_printf("Invalid Process ID!!\n");
+		if (pid < 0)
+			ft_printf("Invalid Process ID!!\n");
+		else
+			ft_printf("Process %d is not exist!!\n", pid);
 		return (1);
 	}
 	send_msg(pid, av[2]);
@@ -42,18 +45,18 @@ int	main(int ac, char **av)
 
 static pid_t	parse_pid(char *s)
 {
-	int		pid;
-	size_t	i;
+	int	i;
+	int	pid;
 
 	i = 0;
 	pid = 0;
 	while (s[i] != '\0')
 	{
-		if (!ft_isdigit(s[i]) || 10 <= i)
-			return (-1);
-		if (INT_MAX / 10 < pid || INT_MAX - (s[i] - '0') < pid * 10)
+		if (!ft_isdigit(s[i]))
 			return (-1);
 		pid = pid * 10 + (s[i] - '0');
+		if (99999 < pid)
+			return (-1);
 		i++;
 	}
 	return ((pid_t)pid);
@@ -102,6 +105,8 @@ static void	send_msg(pid_t pid, char *msg)
 			break ;
 		i++;
 	}
+	if (!g_sigflg)
+		usleep(100);
 	if (g_sigflg)
 		ft_printf("[ Successed to send message. ]\n");
 	else
