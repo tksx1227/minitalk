fa7592e33b358eadd18fc037f30b7db11f50587a
tomori <tmrtkm1227tksx@gmail.com>
2022-02-26 23:54:48 +0900

    [update] Implemented interrupt processing

--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:51:29 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/26 18:28:51 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/26 23:46:37 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	sig_handler(int signum, siginfo_t *info, void *context);
 static void	store_bits(int bit, pid_t client_pid);
 static void	send_signal_to_client(pid_t client_pid);
 
-volatile sig_atomic_t	g_is_processing;
+volatile sig_atomic_t	g_is_interrupted;
 
 int	main(void)
 {
@@ -26,7 +26,7 @@ int	main(void)
 	pid = getpid();
 	ft_printf("PID: %d\n", pid);
 	setup_sigaction(&sig_handler);
-	g_is_processing = 0;
+	g_is_interrupted = 0;
 	while (1)
 		pause();
 	return (0);
@@ -53,15 +53,13 @@ static void	sig_handler(int signum, siginfo_t *info, void *context)
 	static pid_t	client_pid;
 
 	(void)context;
-	if (g_is_processing)
+	if (client_pid != info->si_pid)
 	{
-		if (client_pid != info->si_pid)
-			return ;
-	}
-	else
-	{
-		g_is_processing = 1;
 		client_pid = info->si_pid;
+		if (client_pid != 0)
+		{
+			g_is_interrupted = 1;
+		}
 	}
 	if (signum == SIGUSR1)
 		store_bits(0, client_pid);
@@ -71,26 +69,30 @@ static void	sig_handler(int signum, siginfo_t *info, void *context)
 
 static void	store_bits(int bit, pid_t client_pid)
 {
-	static int				idx;
-	static int				offset;
+	static size_t			offset;
 	static unsigned char	buf[1024];
 
-	buf[idx] |= (bit << offset++);
-	if (CHAR_BIT <= offset)
+	if (g_is_interrupted)
 	{
-		idx++;
-		if (buf[idx - 1] == '\0' || 1000 < idx)
+		buf[offset / CHAR_BIT] = 0;
+		ft_printf("%s", buf);
+		offset = 0;
+		g_is_interrupted = 0;
+	}
+	buf[offset / CHAR_BIT] |= (bit << (offset % CHAR_BIT));
+	offset++;
+	if ((offset % CHAR_BIT) == 0)
+	{
+		if (buf[(offset / CHAR_BIT) - 1] == '\0' || 1000 < (offset / CHAR_BIT))
 		{
 			ft_printf("%s", buf);
-			if (buf[idx - 1] == '\0')
+			if (buf[(offset / CHAR_BIT) - 1] == '\0')
 			{
 				send_signal_to_client(client_pid);
-				g_is_processing = 0;
 			}
-			idx = 0;
+			offset = 0;
 		}
-		buf[idx] = 0;
-		offset = 0;
+		buf[offset / CHAR_BIT] = 0;
 	}
 }
 
