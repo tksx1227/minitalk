efcdfcc37ddafb5969e1df2742ca3e8e17570690
tomori <tmrtkm1227tksx@gmail.com>
2022-02-26 17:17:38 +0900

    [refactor] Remove get_byte_size function

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:52:09 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/25 15:56:07 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/26 17:16:24 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,6 @@
 # define FAILED_SEND_SIGNAL_ERROR "Error: Failed to send signal to process %d.\n"
 # define SUCCESSED_MESSAGE "[ Successed to send message. ]\n"
 # define FAILED_MESSAGE "[ Failed to send message. ]\n"
-
-typedef enum e_masks
-{
-	MASK_1BYTE = 0x80,
-	MASK_2BYTE = 0xE0,
-	MASK_3BYTE = 0xF0,
-	MASK_4BYTE = 0xF8,
-	MASK_5BYTE = 0xFC,
-	MASK_6BYTE = 0xFE,
-}	t_masks;
-
-typedef enum e_prefix
-{
-	PREFIX_1BYTE = 0x00,
-	PREFIX_2BYTE = 0xC0,
-	PREFIX_3BYTE = 0xE0,
-	PREFIX_4BYTE = 0xF0,
-	PREFIX_5BYTE = 0xF8,
-	PREFIX_6BYTE = 0xFC,
-}	t_prefix;
 
 void	setup_sigaction(void (*sig_handler)(int, siginfo_t *, void *));
 
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:51:29 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/25 16:04:07 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/26 17:15:08 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 static void	sig_handler(int signum, siginfo_t *info, void *context);
 static void	store_bits(int bit, pid_t client_pid);
 static void	send_signal_to_client(pid_t client_pid);
-static int	get_byte_size(unsigned char c);
 
 volatile sig_atomic_t	g_is_processing;
 
@@ -57,16 +56,13 @@ static void	store_bits(int bit, pid_t client_pid)
 {
 	static int				idx;
 	static int				offset;
-	static int				byte_size;
 	static unsigned char	buf[1024];
 
 	buf[idx] |= (bit << offset++);
 	if (CHAR_BIT <= offset)
 	{
 		idx++;
-		if (byte_size == 0)
-			byte_size = get_byte_size(buf[idx - 1]);
-		if (buf[idx - 1] == '\0' || (1000 <= idx && byte_size == 1))
+		if (buf[idx - 1] == '\0' || 1000 <= idx)
 		{
 			ft_printf("%s", buf);
 			ft_bzero(buf, idx);
@@ -77,33 +73,8 @@ static void	store_bits(int bit, pid_t client_pid)
 			}
 			idx = 0;
 		}
-		byte_size--;
 		offset = 0;
 	}
-}
-
-static int	get_byte_size(unsigned char c)
-{
-	int	byte_size;
-
-	if ((c & MASK_1BYTE) == PREFIX_1BYTE)
-		byte_size = 1;
-	else if ((c & (unsigned char)MASK_2BYTE) == (unsigned char)PREFIX_2BYTE)
-		byte_size = 2;
-	else if ((c & (unsigned char)MASK_3BYTE) == (unsigned char)PREFIX_3BYTE)
-		byte_size = 3;
-	else if ((c & (unsigned char)MASK_4BYTE) == (unsigned char)PREFIX_4BYTE)
-		byte_size = 4;
-	else if ((c & (unsigned char)MASK_5BYTE) == (unsigned char)PREFIX_5BYTE)
-		byte_size = 5;
-	else if ((c & (unsigned char)MASK_6BYTE) == (unsigned char)PREFIX_6BYTE)
-		byte_size = 6;
-	else
-	{
-		ft_printf(INVALID_BIT_ERROR);
-		exit(1);
-	}
-	return (byte_size);
 }
 
 static void	send_signal_to_client(pid_t client_pid)
