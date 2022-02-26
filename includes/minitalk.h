42af236b61bf35f64e81c5cde1e582fefe9c83f0
tomori <tmrtkm1227tksx@gmail.com>
2022-02-26 17:33:10 +0900

    [refactor] Fixed error messages

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 18:52:09 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/26 17:16:24 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/26 17:32:47 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <sys/types.h>
 # include "../ft_printf/includes/ft_printf.h"
 
-# define SETUP_SIGACTION_ERROR "Error: Setup sigaction is failed.\n"
+# define SETUP_SIGACTION_ERROR "Error: Setup sigaction failed.\n"
 # define INVALID_BIT_ERROR "Error: Received invalid bit data.\n"
 # define INPUT_ARGUMENT_ERROR "Error: Input Error.\n"
 # define USAGE_MESSAGE "Usage: client <PROCESS_ID> <MESSAGES>\n"
 # define INVALID_PROCESS_ERROR "Error: Invalid Process ID.\n"
-# define NOT_EXIST_PROCESS_ERROR "Error: Process %d is not exist.\n"
+# define NOT_EXIST_PROCESS_ERROR "Error: Process %d does not exist.\n"
 # define FAILED_SEND_SIGNAL_ERROR "Error: Failed to send signal to process %d.\n"
 # define SUCCESSED_MESSAGE "[ Successed to send message. ]\n"
 # define FAILED_MESSAGE "[ Failed to send message. ]\n"
